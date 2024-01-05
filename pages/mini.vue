7261c2b3b3b57f6c1aff508f7914509b1d7d7965
Sopheak <spk91011@gmail.com>
2024-01-05 14:39:36 +0700

    docker file

--- pages/mini.vue
+++ pages/mini.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="h-full"></div>
+  <div class="h-full"><input :value=" initData "/></div>
 </template>
 <script setup lang="ts">
 definePageMeta({
   layout: "mobile",
   title: "home",
   keepalive: true,
+});
+const initData = ref<string>("");
+onMounted(() => {
+  initData.value = (window as any).Telegram.WebApp.initData;
 });
 </script>
