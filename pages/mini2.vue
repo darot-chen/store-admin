9b8a2cb96de22bd21863955b91138b5a22cbbe7d
Sopheak <spk91011@gmail.com>
2024-01-10 15:57:57 +0700

    test click

--- pages/mini2.vue
+++ pages/mini2.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <div class="absolute bottom-0 top-0 z-0">
+    <div class="pointer-events-none absolute bottom-0 top-0 z-0">
       <img class="opacity-5" src="/img/chat-bg.jpeg" alt="" />
     </div>
     <div
-      class="absolute bottom-0 top-[50px] z-10 w-full overflow-auto pb-[60px]"
+      class="pointer-events-none absolute bottom-0 top-[50px] z-10 w-full overflow-auto pb-[60px]"
     >
       window.TelegramGameProxy.initParams: {{ initData }}
     </div>
+    <button class="mt-3 bg-black p-2 text-white" @click="handleClick">
+      open something
+    </button>
   </div>
 </template>
 <script setup lang="ts">
@@ -17,6 +20,10 @@ definePageMeta({
   keepalive: true,
 });
 const initData = ref("");
+
+const handleClick = () => {
+  window.location.replace("/mini");
+};
 
 onMounted(() => {
   initData.value = JSON.stringify((window as any).TelegramGameProxy.initParams);
