f58c6c5b265d3a44bd1efb20c36c9bdea3647f48
Punlork <chekpunlork9999@gmail.com>
2024-03-13 15:59:30 +0700

    added toVFixed definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -19,6 +19,12 @@ declare module "#app" {
 
 declare global {
   interface Number {
+    /**
+     * Extends the Number prototype to include a custom method to format numbers with a fixed number of digits after the decimal point,
+     * but if the last two digits are both zero, it returns the number without decimal places.
+     * @param fractionDigits The number of digits to appear after the decimal point. Default is 2.
+     * @returns A string representation of the number with the specified number of digits after the decimal point, or without decimal places if the last two digits are both zero.
+     */
     toVFixed(fractionDigits: number): string;
   }
 }
