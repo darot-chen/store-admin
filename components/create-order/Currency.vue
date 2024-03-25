de6d57b82e1d8d0ee5a7c1d93925bc5a26e89e2c
Darot <64341213+darot-chen@users.noreply.github.com>
2024-03-25 17:19:01 +0700

    Fix: render currency icon

--- components/create-order/Currency.vue
+++ components/create-order/Currency.vue
@@ -6,12 +6,13 @@
       </p>
     </div>
 
-    <div
-      :class="['inline-flex items-center gap-[5px]', disabled && 'disabled']"
-      @click="onClick"
-    >
+    <div :class="['inline-flex items-center gap-[5px]']" @click="onClick">
       <Icon name="ArrowDown" color="#0000001A" size="9" />
-      <Icon v-if="selected?.label" :name="selected?.label" width="16" />
+
+      <img :src="getIcon()" alt="" :width="16" />
+      <!-- <img :src="USDT" alt="" :width="16" />
+      <img :src="USD" alt="" :width="16" />
+      <img :src="PHP" alt="" :width="16" /> -->
     </div>
   </button>
 
@@ -34,18 +35,39 @@
 
 <script setup lang="ts">
 import type { Option } from "~/types/common";
+import CNY from "~/assets/currency/CNY.svg";
+import USDT from "~/assets/currency/USDT.svg";
+import USD from "~/assets/currency/USD.svg";
+import PHP from "~/assets/currency/PHP.svg";
 
 const showPopup = ref(false);
 
 const props = defineProps<{
   option: Option[];
-  modelValue: string;
+  modelValue: number;
   disabled?: boolean;
   title?: string;
 }>();
 
+const getIcon = (): string => {
+  switch (selected.value?.label) {
+    case "CNY":
+      return CNY;
+    case "USDT":
+      return USDT;
+    case "USD":
+      return USD;
+    case "PHP":
+      return PHP;
+    default:
+      return CNY;
+  }
+};
+
 const selected = computed(() => {
-  return props.option.find((item) => item.value === props.modelValue);
+  return props.option.find(
+    (item: Option) => item.value === props.modelValue.toString()
+  );
 });
 
 const emits = defineEmits<{
