3a9766d218c81781e17032986f8700fa4dc86cc2
tolatouchh <142768303+tolatouchh@users.noreply.github.com>
2024-03-27 17:52:49 +0700

    change wording on profile (#273)

--- constants/options/profile.ts
+++ constants/options/profile.ts
@@ -1,6 +1,6 @@
 import type { Option } from "~/types/common";
 
 export const PROFILE_TAB_OPTIONS: Option[] = [
-  { label: "快捷选择", value: "quickSelection" },
-  { label: "固定日期", value: "fixedDate" },
+  { label: "用户模式", value: "quickSelection" },
+  { label: "商户模式", value: "fixedDate" },
 ];
