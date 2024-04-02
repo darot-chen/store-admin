471ab664d6c85accc12cd76893c640d455e35583
Darot <64341213+darot-chen@users.noreply.github.com>
2024-04-02 14:22:59 +0700

    Fix: update report switch

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,11 +1,13 @@
 import { showFailToast } from "vant";
 import { getMe } from "~/api/user";
+import type { Option } from "~/types/common";
 import type { User } from "~/types/user";
 
 export const useAuthStore = defineStore({
   id: "auth-store",
   state: () => ({
     user: undefined as User | undefined,
+    selectedReportTab: undefined as Option | undefined,
   }),
   actions: {
     async getUser() {
